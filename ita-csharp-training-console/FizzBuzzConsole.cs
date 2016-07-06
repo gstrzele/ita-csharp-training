b03293febbb485d33b56ec525e7e0891547fe7ac
Jason Wollam <jason.wollam@agilethought.com>
2016-07-06 10:05:06 -0400

    Adding Console Project

---
+++ ita-csharp-training-console/FizzBuzzConsole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ita_chsharp_training;
+
+namespace ita_csharp_training_console
+{
+    class FizzBuzzConsole
+    {
+        static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzzMain();
+
+            var result = fizzBuzz.ExecuteFizzBuzz();
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
